3616bd4571b7f9e62e1fda57d877e63da5d22892
krisstanket <stankyavichutek@gmail.com>
2019-12-08 14:37:12 +1000

    Переименование класса Теория по классам и объектам

---
+++ addressbook-web-tests/addressbook-web-tests/Figure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class Figure
+    {
+        private bool coloured = false;
+
+        public bool Coloured
+        {
+            get
+            {
+                return coloured;
+            }
+            set
+            {
+                coloured = value;
+            }
+        }
+    }
+}
