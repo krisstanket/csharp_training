7a4d57413d99afa7731b00750b27b1466ee108ec
krisstanket <stankyavichutek@gmail.com>
2019-12-08 15:23:38 +1000

    Задание 3: создание вспомогательных методов и классов

---
+++ addressbook-web-tests/addressbook-web-tests/GroupData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressBookTests
+{
+    class GroupData
+    {
+        private string groupName;
+        private string header = "";
+        private string footer = "";
+
+        public GroupData (string groupName)
+        {
+            this.groupName = groupName;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return groupName;
+            }
+            set
+            {
+                groupName = value;
+            }
+        }
+
+        public string Header
+        {
+            get
+            {
+                return header;
+            }
+            set
+            {
+                header = value;
+            }
+        }
+
+        public string Footer
+        {
+            get
+            {
+                return footer;
+            }
+            set
+            {
+                footer = value;
+            }
+        }
+    }
+}
