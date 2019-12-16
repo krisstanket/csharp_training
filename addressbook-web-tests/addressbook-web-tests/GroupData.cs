43c978441a0e5e8b8fde50840b6ed2824fc04f74
krisstanket <stankyavichutek@gmail.com>
2019-12-16 22:51:24 +1000

    Задание 5: Создание базового класса для тестовых классов

--- addressbook-web-tests/addressbook-web-tests/GroupData.cs
+++ addressbook-web-tests/addressbook-web-tests/GroupData.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebAddressBookTests
 {
-    class GroupData
+    public class GroupData
     {
         private string groupName;
         private string header = "";
--- addressbook-web-tests/addressbook-web-tests/GroupRemovalTest.cs
+++ addressbook-web-tests/addressbook-web-tests/GroupRemovalTest.cs
@@ -7,22 +7,17 @@ using NUnit.Framework;
 namespace WebAddressBookTests
 {
     [TestFixture]
-    public class GroupCreationTests : TestBase
+    public class GroupRemovalTests : TestBase
     {
         [Test]
-        public void GroupCreationTest()
+        public void GroupRemovalTest()
         {
             OpenHomePage();
             Login(new AccountData("admin", "secret"));
             GoToGroupsPage();
-            InitGroupCreation();
-            GroupData group = new GroupData("testgroup");
-            group.Header = "header";
-            group.Footer = "footer";
-            FillGroupForm(group);
-            SubmitGroupCreation();
+            SelectGroup(1);
+            ClickRemoveGroupButton();
             ReturnToGroupsPage();
-            Logout();
-        } 
+        }            
     }
 }
