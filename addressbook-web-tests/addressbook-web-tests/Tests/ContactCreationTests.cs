fce51aed950702090c5722f1b186b39833209631
krisstanket <stankyavichutek@gmail.com>
2020-01-30 22:24:42 +1000

    Задание 15: загрузка тестовых данных из файла

--- addressbook-web-tests/addressbook-web-tests/Tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/ContactCreationTests.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace WebAddressBookTests
@@ -21,7 +25,21 @@ namespace WebAddressBookTests
             return contacts;
         }
 
-        [Test, TestCaseSource("RandomContactDataProvider")]
+        public static IEnumerable<ContactData> ContactDataFromXmlFile()
+        {
+            return (List<ContactData>)
+                 new XmlSerializer(typeof(List<ContactData>))
+                 .Deserialize(new StreamReader(@"contacts.xml"));
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<ContactData>>(
+                File.ReadAllText(@"contacts.json")
+                );
+        }
+
+        [Test, TestCaseSource("ContactDataFromJsonFile")]
         public void ContactCreationTest(ContactData contact)
         {
             List<ContactData> oldContacts = appManager.Contacts.GetContactsList();
